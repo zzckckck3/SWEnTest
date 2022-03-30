506a0c0832db26af523fc86451e7ff6a4679bfc6
DESKTOP-45RVDID\leej <zzckckck3@naver.com>
2022-03-30 14:44:38 +0900

    first commit

---
+++ 1.py
@@ -0,0 +1,23 @@
+a = str(input())
+
+
+def solution(a):
+    stack = []
+    if a[0] == ')':
+        return False
+    for i in a:
+        if i == '(':
+            stack.append(i)
+        else:
+            stack.pop()
+
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+
+if solution(a) == True:
+    print('true')
+else:
+    print('false')
